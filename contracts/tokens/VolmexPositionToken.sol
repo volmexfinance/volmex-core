e570b9f98d7aa96a9d9804d892eea5ba6f481f91
Ayush Tiwari <ayush@volmexlabs.com>
2021-05-12 20:21:53 +0530

    VPT-01: Change the VOLMEX_PROTOCOL_ROLE specifier to private.

--- contracts/tokens/VolmexPositionToken.sol
+++ contracts/tokens/VolmexPositionToken.sol
@@ -15,7 +15,7 @@ contract VolmexPositionToken is
     ERC20PausableUpgradeable
 {
     // Position token role, calculated as keccak256("VOLMEX_PROTOCOL_ROLE")
-    bytes32 public constant VOLMEX_PROTOCOL_ROLE =
+    bytes32 private constant VOLMEX_PROTOCOL_ROLE =
         0x33ba6006595f7ad5c59211bde33456cab351f47602fc04f644c8690bc73c4e16;
 
     /**
